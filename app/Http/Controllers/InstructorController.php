64bc2ef0d62784b9900cad7f8c610eec3a83b954
Febrian Bayu Nugroho <fbrnngrh@gmail.com>
2023-11-28 23:08:50 +0800

    feat: make same feature for instructor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -3,12 +3,137 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\RedirectResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class InstructorController extends Controller
 {
     //
     public function InstructorDashboard()
     {
-        return view('instructor.instructor_dashboard');
+        return view('instructor.index');
     }
+
+    public function InstructorLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/instructor/login');
+    }
+
+    public function InstructorLogin()
+    {
+        return view('instructor.instructor_login');
+    }
+
+    public function InstructorProfile()
+    {
+
+        $id = Auth::user()->id;
+
+        $profileData = User::find($id);
+
+        return view('instructor.instructor_profile_view', compact('profileData'));
+    }
+
+    public function InstructorProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+    
+        $profileData = User::find($id);
+    
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'username' => 'nullable|string|max:255|unique:users,username,' . $id,
+            'email' => 'required|email|max:255|unique:users,email,' . $id,
+            'phone' => 'nullable|string|max:20',
+            'address' => 'nullable|string|max:255',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
+        // Redirect back with errors if validation fails
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    
+        // Update profile data
+        $profileData->name = $request->name;
+        $profileData->username = $request->username;
+        $profileData->email = $request->email;
+        $profileData->phone = $request->phone;
+        $profileData->address = $request->address;
+    
+        // Process and save the new photo
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            @unlink(public_path('upload/instructor_images/' . $profileData->photo));
+            $imageName = date('YmdHi') . '-' . $image->getClientOriginalName();
+            $image->move(public_path('upload/instructor_images'), $imageName);
+            $profileData->photo = $imageName;
+        }
+    
+        $profileData->save();
+
+        $notification = array(
+            'message' => 'Instructor Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+    
+        return redirect()->back()->with($notification);
+    }
+
+    public function InstructorChangePassword()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('instructor.instructor_change_password', compact('profileData'));
+    }
+
+    public function InstructorPasswordUpdate(Request $request)
+    {
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed|min:8',
+        ]);
+
+        // Redirect back with errors if validation fails
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Check if old password matches
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            $notification = array(
+                'message' => 'Old Password Does Not Match',
+                'alert-type' => 'error'
+            );
+
+            return redirect()->back()->with($notification);
+        }
+
+        // Update password 
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
+// Admin Routes
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 // Admin group Middleware
 Route::middleware(['auth', 'roles:admin'])->group(function () {
@@ -39,11 +39,20 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
 
 });
 
+// Instructor Routes
+Route::get('/instructor/login', [InstructorController::class, 'InstructorLogin'])->name('instructor.login');
 // Instructor group Middleware
 Route::middleware(['auth', 'roles:instructor'])->group(function () {
     Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+    Route::get('/instructor/logout', [InstructorController::class, 'InstructorLogout'])->name('instructor.logout');
+    Route::get('/instructor/profile', [InstructorController::class, 'InstructorProfile'])->name('instructor.profile');
+    Route::post('/instructor/profile/store', [InstructorController::class, 'InstructorProfileStore'])->name('instructor.profile.store');
+    Route::get('/instructor/change/password', [InstructorController::class, 'InstructorChangePassword'])->name('instructor.change.password');
+    Route::post('/instructor/password/update', [InstructorController::class, 'InstructorPasswordUpdate'])->name('instructor.password.update');
 });
 
